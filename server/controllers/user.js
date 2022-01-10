8b94afe675a24acaeb39719b9021ec36b3b60074
Joey Davidson <j.davidson18@gmail.com>
2022-01-10 11:57:37 -0700

    routes, model, and controller set up for User on backend, middleware next

---
+++ server/controllers/user.js
@@ -0,0 +1,69 @@
+//hash the password for protection against hacking
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+import User from "../models/user.js";
+
+export const signin = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const existingUser = await User.findOne({ email });
+
+    if (!existingUser)
+      return res.status(404).json({ message: "User doesn't exist" });
+
+    const isPasswordCorrect = await bcrypt.compare(
+      password,
+      existingUser.password
+    );
+    if (!isPasswordCorrect)
+      return res.status(400).json({ message: "Invalid credentials" });
+
+    const token = jwt.sign(
+      { email: existingUser.email, id: existingUser._id },
+      //secret string only I know, put in separate ENV file
+      "test",
+      //options object
+      { expiresIn: "1h" }
+    );
+    res.status(200).json({ result: existingUser.token });
+  } catch (error) {
+    res.status(500).json({ message: "Something went wrong." });
+  }
+};
+
+export const signup = async (req, res) => {
+  const { email, password, confirmPassword, firstName, lastName } = req.body;
+
+  try {
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser)
+      return res.status(400).json({ message: "User already exists" });
+
+    if (password !== confirmPassword)
+      return res.status(400).json({ message: "Passwords don't match" });
+
+    //hash password,                          salt= level of difficulty wanted to use to hash password, 12 'standard'
+    const hashedPassword = await bcrypt.hash(password, 12);
+
+    const result = await User.create({
+      email,
+      password: hashedPassword,
+      name: `${firstName} ${lastName}`,
+    });
+
+    const token = jwt.sign(
+      { email: result.email, id: result._id },
+      //secret string only I know, put in separate ENV file
+      "test",
+      //options object
+      { expiresIn: "1h" }
+    );
+
+    res.status(200).json({ result, token });
+  } catch (error) {
+    res.stattus(500).json({ message: "Something went wrong" });
+  }
+};
